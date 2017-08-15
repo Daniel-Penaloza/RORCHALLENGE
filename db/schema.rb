cc5b5e1a28486087cf68f21c48c73e0860a9ebc3
Ollie <oliverbucks@gmail.com>
2017-08-15 17:44:38 +0000

    add products to table

--- db/schema.rb
+++ db/schema.rb
@@ -21,11 +21,13 @@ ActiveRecord::Schema.define(version: 20170816020612) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.float "price"
+    t.string "summary"
     t.string "description"
     t.string "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
