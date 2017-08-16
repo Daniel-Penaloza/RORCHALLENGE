70c3cc85ec984e52962b7d0ddcbadb5a5312ed57
Ollie <oliverbucks@gmail.com>
2017-08-16 21:37:05 +0000

    .

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,54 @@
+class CategoriesController < ApplicationController
+    
+    before_action :set_category, only: [:edit, :update, :show]
+
+  def new
+    @category = Category.new
+  end
+  
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to category_path(@category)
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:success] = "Category successfully updated"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @category_goals = @category.goals.paginate(page: params[:page], per_page: 5)
+    
+  end
+  
+  def index
+    #@categories = Category.paginate(page: params[:page], per_page: 5)
+    @categories = Category.all
+  end
+  
+  def destroy
+  
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])  
+  end
+  
+  def category_params
+    params.require(:category).permit(:name)
+  end
+  
+end
---
+++ db/migrate/20170815183100_add_summary_to_products.rb
@@ -0,0 +1,6 @@
+class AddSummaryToProducts < ActiveRecord::Migration[5.1]
+  def change
+    
+    add_column :products, :summary, :text
+  end
+end
