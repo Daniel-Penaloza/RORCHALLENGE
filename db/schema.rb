c0de415e147f3b6ae3d9dbb3b422388e9257966a
Ollie <oliverbucks@gmail.com>
2017-08-17 09:41:07 +0000

    .

--- db/schema.rb
+++ db/schema.rb
@@ -21,13 +21,12 @@ ActiveRecord::Schema.define(version: 20170816020612) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.float "price"
-    t.string "summary"
     t.string "description"
     t.string "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.string "image"
+    t.text "summary"
   end
 
   create_table "users", force: :cascade do |t|
