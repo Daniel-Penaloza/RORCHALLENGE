1077f48a6579241e6e18fb7ffce2d40b9c715061
Daniel Penaloza <danielpenalozatj@gmail.com>
2017-08-17 15:14:17 -0700

    products index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
-	
+	before_action :set_user, only: [:show, :edit, :create, :update]
+
 	def index
 		@users = User.all
 	end
 
 	def show
-		@user = User.find(params[:id])
 	end
+
+	def edit
+	end
+
+	private
+		def set_user
+			@user = User.find(params[:id])
+		end
 end
