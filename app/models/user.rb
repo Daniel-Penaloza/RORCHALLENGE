2b1e0bcec1fd8ebce2c394499c216b0413dda6f2
Ollie <oliverbucks@gmail.com>
2017-08-20 11:49:47 +0000

    added confirmed emails & nav change

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :confirmable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
     has_many :products
 	has_many :categories, :through  => :products
 
 	def full_name
-		(first_name + " " + last_name).titleize
+    "#{first_name} #{last_name}" 
 	end
 
 	mount_uploader :image, UserImageUploader
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816020612) do
+ActiveRecord::Schema.define(version: 20170820104023) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -47,6 +47,10 @@ ActiveRecord::Schema.define(version: 20170816020612) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.string "phone"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
